6cb14e90fcfdf40bbb376ceb1a95f1887e793363
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-11-01 20:09:33 +0100

    implement additional features for web app

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
-	// decrement,
+	decrement,
 	increment,
 	incrementByAmount,
 	incrementAsync,
